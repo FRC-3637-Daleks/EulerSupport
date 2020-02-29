4434c44a6aad1d7d5f902f1437af68d27504617b
Rsaperstein <33642090+Rsaperstein@users.noreply.github.com>
2020-02-29 14:44:37 -0500

    Rounded angle and print whether or not sensor is level (within 8 degrees in either direction).

--- angleSensor.py
+++ angleSensor.py
@@ -14,6 +14,8 @@ sensor = LSM6DSOX(i2c)
 
 i = 0
 
+isLevel = False
+
 while i == 0:
 #	sd.putNumber("X Angle", sensor.gyro[0])
 #	sd.putNumber("Y Angle", sensor.gyro[1])
@@ -21,16 +23,35 @@ while i == 0:
 #	sd.putNumber("X Accel", sensor.acceleration[0])
 #	sd.putNumber("Y Accel", sensor.acceleration[1])
 #	sd.putNumber("Z Accel", sensor.acceleration[2])
-
 	z = sensor.acceleration[2]
 	x = sensor.acceleration[0]
 	g = numpy.sqrt(z * z + x * x)
 	angle = numpy.arcsin(z / g)
 	angle *= 180 / numpy.pi
+
 	if x < 0:
 		angle *= -1
-	#print("The angle is ", angle)
-	#print(sensor.acceleration[0], " ", sensor.acceleration[1], " ", sensor.acceleration[2])
-	sd.putNumber("Angle", angle)
-	time.sleep(0.05)
 
+	if angle < 0:
+		angle = 90 + angle
+	else:
+		angle = 90 - angle
+
+	angleRound = round(angle, 1)
+	multiplier = 10 ** 1
+	angleRound = int(angleRound * multiplier) / multiplier
+
+	if x < 0:
+		angle *= -1
+		angleRound *= -1
+
+	if angleRound < 8 and angleRound > -8:
+		isLevel = True
+	else:
+		isLevel = False
+	#print("The angle is ", angle)
+	#print(sensor.acceleration[0], " ", senso r.acceleration[1], " ", sensor.acceleration[2])
+	sd.putNumber("Angle", angle)
+	sd.putNumber("Angle Rounded", angleRound)
+	sd.putBoolean("Is Level", isLevel)
+	time.sleep(0.05)
